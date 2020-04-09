b14ba2905e2aad2d6687f2bb284db8eca004cb7a
Federico Fuhr <federico.fuhr@live.com>
2020-04-09 19:00:15 -0300

    añadida funcionalidades hasta el punto 7 sin el punto 5

--- tp1/src/tp1/Main.java
+++ tp1/src/tp1/Main.java
@@ -8,21 +8,43 @@ public class Main {
 		
 		MySimpleLinkedList list = new MySimpleLinkedList();
 		
-		list.insertFront(1); //4
-		list.insertFront(3); //3
-		list.insertFront(5); //2
-		list.insertFront(7); //1
-		list.insertFront(9); //0
+		list.insertFront(1); 
+		list.insertFront(7); 
+		list.insertFront(4); 		
+		list.insertFront(10);
+		list.insertFront(6);
+		list.insertFront(11);
+		list.insertFront(5);
+		list.insertFront(27);
+		list.insertFront(26);
+		
 		
 		int index = 3;
 		Object o1 = 5;
-		Object o2 = 10;
+		Object o2 = 100;
 		
 		System.out.println("LISTA: " + list);
-		System.out.println("LA CANTIDAD ES: " + list.size);
-		System.out.println("ELEMENTO EN EL LUGAR " + index + " : " + list.get(index));
-		System.out.println("ESTA EL ELEMENTO " + o1 + ": " + list.indexOf(o1));
-		System.out.println("ESTA EL ELEMENTO " + o2 + ": " + list.indexOf(o2));
+//		System.out.println("LA CANTIDAD ES: " + list.size);
+//		System.out.println("ELEMENTO EN EL LUGAR " + index + " : " + list.get(index));
+//		System.out.println("ESTA EL ELEMENTO " + o1 + ": " + list.indexOf(o1));
+//		System.out.println("ESTA EL ELEMENTO " + o2 + ": " + list.indexOf(o2));
+		
+		
+		MySimpleLinkedList list2 = new MySimpleLinkedList();
+		
+		list2.insertFront(57); 
+		list2.insertFront(68); 
+		list2.insertFront(4); 		
+		list2.insertFront(1);
+		list2.insertFront(6);
+		list2.insertFront(25);
+		list2.insertFront(45);
+		list2.insertFront(27);
+		list2.insertFront(26);
+		
+		System.out.println("LISTA 2: " + list2);
+		System.out.println("NUEVA LISTA MISMOS ELEMENTOS ORDENADA: " + list.getOrderList(list2).toString());
+		System.out.println("NUEVA LISTA CON LA DIFERENCIA: " + list.difference(list2).toString());
 		System.out.println("-------------------------------------------------");
 		
 		/////////////////////////////////////////////////////////////////////////
@@ -46,7 +68,8 @@ public class Main {
 		System.out.println("TOPE: " + p.top());
 		
 		p.reverse();
-		System.out.println("PILA: " + p.toString());
+		System.out.println("PILA CON REVERSA: " + p.toString());
+		System.out.println("-------------------------------------------------");
 		
 		/////////////////////////////////////////////////////////////////////////
 		
--- tp1/src/tp1/MySimpleLinkedList.java
+++ tp1/src/tp1/MySimpleLinkedList.java
@@ -1,15 +1,15 @@
 package tp1;
 
-import java.util.Iterator;
-
-public class MySimpleLinkedList implements Iterable<Object>{
+public class MySimpleLinkedList{
 	
 	protected Node first;
+	protected Node last;
 	protected int size;
 	
 	
 	public MySimpleLinkedList() {
 		this.first = null;
+		this.last = null;
 		this.size = 0;
 	}
 	
@@ -18,6 +18,42 @@ public class MySimpleLinkedList implements Iterable<Object>{
 		tmp.setNext(this.first);
 		this.first = tmp;
 		this.size++;
+	}
+	
+	public void insertLast(Object o) {
+		Node tmp = new Node(o, null);
+		this.last.setNext(tmp);
+		tmp.setNext(this.last);
+		this.last = tmp;
+		this.size++;
+	}
+	
+	public void insertOrdered(Object o) {
+		if (!this.isEmpty()) {
+			if ((Integer)this.first.getInfo() > (Integer)o) {
+				insertFront(o);
+			} else if ((Integer)this.last.getInfo() < (Integer)o) {
+				insertLast(o);
+			} else {
+				Node nuevo = new Node(o, null);
+				
+				Node n = this.first;
+				Node n2 = null;
+				
+				while ((n != null) && ((Integer)n.getInfo() < (Integer)o)) {
+					n2 = n;
+					n = n.getNext();
+				}
+				
+				n2.setNext(nuevo);
+				nuevo.setNext(n);
+				
+				this.size++;
+			}
+		} else {
+			this.insertFront(o);
+			this.last = first;
+		}
 	}
 	
 	public Object extractFront() {
@@ -53,7 +89,7 @@ public class MySimpleLinkedList implements Iterable<Object>{
 		// TODO
 		if (this.size > index) {
 			Node n = this.first;
-			int i = 1;
+			int i = 0;
 			while (i != index) {
 				n = n.getNext();
 				i++;
@@ -108,11 +144,57 @@ public class MySimpleLinkedList implements Iterable<Object>{
 		
 		this.first = l.first;
 	}
-
-	@Override
-	public Iterator<Object> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public MySimpleLinkedList getOrderList(MySimpleLinkedList l) {
+		
+		MySimpleLinkedList r = new MySimpleLinkedList();
+		
+		int i = 0;
+		
+		while (i < this.size()) {
+			int j = 0;
+			while (j < l.size()) {
+				if (this.get(i).equals(l.get(j))) {
+					r.insertOrdered(this.get(i));
+				}
+				j++;
+			}
+			i++;
+		}
+		
+		return r;
+		
+	}
+	
+	public boolean contains(Object o) {
+		boolean r = false;
+		
+		Node n = this.first;
+		
+		while ((n != null) && (!r)) {
+			if (n.getInfo().equals(o))
+				r = true;
+			n = n.getNext();
+		}
+		
+		return r;
+	}
+	
+	public MySimpleLinkedList difference(MySimpleLinkedList l) {
+		MySimpleLinkedList r = new MySimpleLinkedList();
+		
+		int i = 0;
+		
+		while (i < this.size()) {
+			
+			if (!l.contains(this.get(i))) {
+				r.insertOrdered(this.get(i));
+			}
+				
+			i++;
+		}
+		
+		return r;
 	}
 
 }
